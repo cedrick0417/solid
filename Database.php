7e276899f532de207a69396fd8bcebc415384c5d
Cedrick Garcia <cedrick.garcia@ippphilippines.com>
2023-02-16 16:24:47 +0800

    Change to PDO

--- Database.php
+++ Database.php
@@ -5,24 +5,61 @@ class Database {
     private $password = "";
     private $database = "crud-api-pdo";
 
-    public function connect() {
-        $connection = new mysqli($this->host, $this->username, $this->password, $this->database);
-        echo "connected";
-
-        if ($connection->connect_error) {
-            die("Connection failed: " . $connection->connect_error);
+    public $conn;
+     public function getConnection(){
+            $this->conn = null;
+            try{
+                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->database, $this->username, $this->password);
+                $this->conn->exec("set names utf8");
+                echo "Database connected";
+            }catch(PDOException $exception){
+                echo "Database could not be connected: " . $exception->getMessage();
+            }
+            return $this->conn;
         }
 
-        return $connection;
-    }
+    // public function connect() {
+    //     $connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+    //     echo "connected";
 
-    public function executeQuery($sql) {
-        $connection = $this->connect();
+    //     if ($connection->connect_error) {
+    //         die("Connection failed: " . $connection->connect_error);
+    //     }
 
-        $result = $connection->query($sql);
+    //     return $connection;
+    // }
 
-        $connection->close();
+    // public function executeQuery($sql) {
+    //     $connection = $this->connect();
 
-        return $result;
-    }
+    //     $result = $connection->query($sql);
+
+    //     $connection->close();
+
+    //     return $result;
+    // }
 }
+// <?php 
+//     class Database {
+//         private $host = "127.0.0.1";
+//         private $database_name = "phpapidb";
+//         private $username = "root";
+//         private $password = "eP#A2PMsY^a4";
+
+//         public $conn;
+
+//         public function getConnection(){
+//             $this->conn = null;
+//             try{
+//                 $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->database_name, $this->username, $this->password);
+//                 $this->conn->exec("set names utf8");
+//             }catch(PDOException $exception){
+//                 echo "Database could not be connected: " . $exception->getMessage();
+//             }
+//             return $this->conn;
+//         }
+//     }  
+// 
+// For Testing
+    // $database = new Database();
+    // $db = $database->getConnection();
